014a83469b0327d684984740de10844d143001cb
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-01-02 14:18:24 -0700

    Rename selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({ 
   moduleId: module.id,
-  selector: 'app-container',
+  selector: 'app-component',
   templateUrl: 'app.component.html'
 })
 export class AppComponent { 
